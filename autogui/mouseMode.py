9de22ab5349e80d03c6791bc3d82566cb851fe4b
nostopp <1141107220@qq.com>
2025-01-22 18:39:47 +0800

    优化鼠标坐标模式

--- autogui/mouseMode.py
+++ autogui/mouseMode.py
@@ -4,12 +4,20 @@ import pyautogui
 
 class MouseMode:
     def __init__(self):
+        self._pressCount = 0
+        self._lastPos = None
         keyboard.on_release(self.onRelease)
 
     def onRelease(self, key):
         if key.name == 'space':
             pos = pyautogui.position()
             print(f'鼠标位置: {pos}')
+
+            self._pressCount += 1
+            if self._pressCount >= 2:
+                self._pressCount = 0
+                print(f'鼠标位置相差: {pos.x - self._lastPos.x}, {pos.y - self._lastPos.y}')
+            self._lastPos = pos
         
     def Update(self):
         time.sleep(1)
