2920b3bb600e2ce04b20c0936f274ad18d2ab78f
nostopp <1141107220@qq.com>
2025-05-16 17:12:23 +0800

    windows下截图适配多屏幕

--- autogui/__init__.py
+++ autogui/__init__.py
@@ -1,3 +1,9 @@
+import sys
+if sys.platform == "win32":
+    import pyscreeze
+    from functools import partial
+    pyscreeze.screenshot = partial(pyscreeze._screenshot_win32, allScreens=True)
+
 from .parser import GetCsv
 from .ocr import OCR
 from .autoOperator import AutoOperator
